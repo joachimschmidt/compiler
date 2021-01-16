bc95f1db9a0abcf1adccc0962c63431dbea049b5
Joachim <joachim.schmidt@wp.pl>
2021-01-16 15:59:41 +0100

    working automatic tests

--- test.py
+++ test.py
@@ -26,9 +26,39 @@ class GotfrydTests(unittest.TestCase):
 
 class GebalaTests(unittest.TestCase):
     def test_compiler(self):
-        for i, file in enumerate(os.listdir("tests_gebala")):
+        files = [f for f in os.listdir("tests_gebala") if os.path.isfile(os.path.join("tests_gebala", f))]
+        for i, file in enumerate(files):
             with self.subTest(i=i):
+                cost = 0
                 compiler = Compiler("tests_gebala/{}".format(file), "out.mr")
+                compiler.read_from_file()
+                compiler.compile()
+                self.assertTrue(compiler.write_to_file())
+                inputs_file = open("tests_gebala/in/{}".format(file), "r")
+                inputs = inputs_file.read().split("X")
+                inputs_file.close()
+                inputs = [x.strip().split() for x in inputs]
+                corrects_file = open("tests_gebala/correct/{}".format(file), "r")
+                corrects = corrects_file.read().split("X")
+                corrects_file.close()
+                corrects = [x.strip().split() for x in corrects]
+                for i, data_set in enumerate(inputs):
+                    correct = corrects[i]
+                    os.system("echo \"{}\" | ./maszyna-wirtualna-cln-test out.mr > out.txt".format(" ".join(data_set)))
+                    result_file = open("out.txt", "r")
+                    result = result_file.read().split()
+                    result_file.close()
+                    cost += int(result[-1])
+                    result.pop()
+                    self.assertEqual(result, correct)
+
+
+
+class CrazyHajdukTests(unittest.TestCase):
+    def test_compiler(self):
+        for i, file in enumerate(os.listdir("tests_crazy")):
+            with self.subTest(i=i):
+                compiler = Compiler("tests_crazy/{}".format(file), "out.mr")
                 compiler.read_from_file()
                 compiler.compile()
                 self.assertTrue(compiler.write_to_file())
