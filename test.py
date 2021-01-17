bb63ccfb3cbd4a408fe1998caae9f7dafcdb0157
Joachim <joachim.schmidt@wp.pl>
2021-01-17 14:38:08 +0100

    added saving costs to csv

--- test.py
+++ test.py
@@ -1,16 +1,31 @@
+import csv
 import os
 import unittest
 
 from compiler import Compiler
 
+cost_full = []
+
+
+def stop_test_run(self):
+    csv_columns = ['name', 'cost']
+    costs_file = open("costs/full.csv", "w")
+    writer = csv.DictWriter(costs_file, csv_columns)
+    writer.writeheader()
+    for data in cost_full:
+        writer.writerow(data)
+    costs_file.close()
+
+
+setattr(unittest.TestResult, 'stopTestRun', stop_test_run)
+
 
 class BauerTests(unittest.TestCase):
     def test_compiler(self):
+        costs = []
         files = [f for f in os.listdir("tests_bauer") if os.path.isfile(os.path.join("tests_bauer", f))]
         for i, file in enumerate(files):
             with self.subTest(i=i):
-                # print("i: {} file: tests_bauer/{}".format(i, file))
-                cost = 0
                 compiler = Compiler("tests_bauer/{}".format(file), "out.mr")
                 compiler.read_from_file()
                 compiler.compile()
@@ -24,25 +39,34 @@ class BauerTests(unittest.TestCase):
                 corrects_file.close()
                 corrects = [x.strip().split() for x in corrects]
                 for i, data_set in enumerate(inputs):
-                    # print(data_set)
                     correct = corrects[i]
                     os.system("echo \"{}\" | ./maszyna-wirtualna-cln-test out.mr > out.txt".format(" ".join(data_set)))
                     result_file = open("out.txt", "r")
                     result = result_file.read().split()
                     result_file.close()
-                    cost += int(result[-1])
+                    cost = int(result[-1])
+                    costs.append({"name": file, "cost": cost})
                     result.pop()
-                    # print(result)
-                    # print(correct)
                     self.assertEqual(result, correct)
+        sum_of_costs = 0
+        for cost in costs:
+            sum_of_costs += cost["cost"]
+        csv_columns = ['name', 'cost']
+        costs_file1 = open("costs/bauer.csv", "w")
+        writer = csv.DictWriter(costs_file1, csv_columns)
+        writer.writeheader()
+        for data in costs:
+            writer.writerow(data)
+        costs_file1.close()
+        cost_full.append({"name": "bauer", "cost": sum_of_costs})
 
 
 class SchmidtTests(unittest.TestCase):
     def test_compiler(self):
+        costs = []
         files = [f for f in os.listdir("tests_schmidt") if os.path.isfile(os.path.join("tests_schmidt", f))]
         for i, file in enumerate(files):
             with self.subTest(i=i):
-                cost = 0
                 compiler = Compiler("tests_schmidt/{}".format(file), "out.mr")
                 compiler.read_from_file()
                 compiler.compile()
@@ -61,17 +85,29 @@ class SchmidtTests(unittest.TestCase):
                     result_file = open("out.txt", "r")
                     result = result_file.read().split()
                     result_file.close()
-                    cost += int(result[-1])
+                    cost = int(result[-1])
+                    costs.append({"name": file, "cost": cost})
                     result.pop()
                     self.assertEqual(result, correct)
+        sum_of_costs = 0
+        for cost in costs:
+            sum_of_costs += cost["cost"]
+        csv_columns = ['name', 'cost']
+        costs_file1 = open("costs/schmidt.csv", "w")
+        writer = csv.DictWriter(costs_file1, csv_columns)
+        writer.writeheader()
+        for data in costs:
+            writer.writerow(data)
+        costs_file1.close()
+        cost_full.append({"name": "schmidt", "cost": sum_of_costs})
 
 
 class GotfrydTests(unittest.TestCase):
     def test_compiler(self):
+        costs = []
         files = [f for f in os.listdir("tests_gotfryd") if os.path.isfile(os.path.join("tests_gotfryd", f))]
         for i, file in enumerate(files):
             with self.subTest(i=i):
-                cost = 0
                 compiler = Compiler("tests_gotfryd/{}".format(file), "out.mr")
                 compiler.read_from_file()
                 compiler.compile()
@@ -90,17 +126,29 @@ class GotfrydTests(unittest.TestCase):
                     result_file = open("out.txt", "r")
                     result = result_file.read().split()
                     result_file.close()
-                    cost += int(result[-1])
+                    cost = int(result[-1])
+                    costs.append({"name": file, "cost": cost})
                     result.pop()
                     self.assertEqual(result, correct)
+        sum_of_costs = 0
+        for cost in costs:
+            sum_of_costs += cost["cost"]
+        csv_columns = ['name', 'cost']
+        costs_file1 = open("costs/gotfryd.csv", "w")
+        writer = csv.DictWriter(costs_file1, csv_columns)
+        writer.writeheader()
+        for data in costs:
+            writer.writerow(data)
+        costs_file1.close()
+        cost_full.append({"name": "gotfryd", "cost": sum_of_costs})
 
 
 class GebalaTests(unittest.TestCase):
     def test_compiler(self):
+        costs = []
         files = [f for f in os.listdir("tests_gebala") if os.path.isfile(os.path.join("tests_gebala", f))]
         for i, file in enumerate(files):
             with self.subTest(i=i):
-                cost = 0
                 compiler = Compiler("tests_gebala/{}".format(file), "out.mr")
                 compiler.read_from_file()
                 compiler.compile()
@@ -119,9 +167,21 @@ class GebalaTests(unittest.TestCase):
                     result_file = open("out.txt", "r")
                     result = result_file.read().split()
                     result_file.close()
-                    cost += int(result[-1])
+                    cost = int(result[-1])
+                    costs.append({"name": file,"cost":cost})
                     result.pop()
                     self.assertEqual(result, correct)
+        sum_of_costs = 0
+        for  cost in costs:
+            sum_of_costs += cost["cost"]
+        csv_columns = ['name', 'cost']
+        costs_file1 = open("costs/gebala.csv", "w")
+        writer = csv.DictWriter(costs_file1, csv_columns)
+        writer.writeheader()
+        for data in costs:
+            writer.writerow(data)
+        costs_file1.close()
+        cost_full.append({"name":"gebala","cost":sum_of_costs})
 
 
 class CrazyHajdukTests(unittest.TestCase):
